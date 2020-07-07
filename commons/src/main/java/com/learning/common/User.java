e0c9bd29eec424ef6136871402309cd45839ab57
薛重阳 <>
2020-07-07 14:36:48 +0800

    update

--- commons/src/main/java/com/learning/common/User.java
+++ commons/src/main/java/com/learning/common/User.java
@@ -1,49 +1,18 @@
 package com.learning.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@AllArgsConstructor
+@Setter
+@Getter
 public class User {
 	private Integer userid;
 	private String username;
 	private String mobile;
 	private Date startTime;
 
-	public User(Integer userid, String username, String mobile, Date time){
-		this.userid = userid;
-		this.username = username;
-		this.mobile = mobile;
-		this.setStartTime(time);
-	}
-	
-	public Integer getUserid() {
-		return userid;
-	}
-
-	public void setUserid(Integer userid) {
-		this.userid = userid;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getMobile() {
-		return mobile;
-	}
-
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
 }
