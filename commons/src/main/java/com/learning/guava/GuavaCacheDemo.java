5133a84924c55917c14f22dffb73b2c25df9a625
睿元 <ruiyuan.xcy@alibaba-inc.com>
2025-06-06 10:53:29 +0800

    feat: 添加矩阵螺旋打印功能

--- commons/src/main/java/com/learning/guava/GuavaCacheDemo.java
+++ commons/src/main/java/com/learning/guava/GuavaCacheDemo.java
@@ -28,6 +28,7 @@ public class GuavaCacheDemo {
             .expireAfterWrite(1, TimeUnit.DAYS)
             .expireAfterAccess(1, TimeUnit.DAYS)
             .refreshAfterWrite(1, TimeUnit.MINUTES)
+            
             .build(new CacheLoader<String, String>() {
                 @Override
                 public String load(String key) throws Exception {
@@ -70,5 +71,65 @@ public class GuavaCacheDemo {
             TimeUnit.MILLISECONDS.sleep(400);
         }
         localCache.invalidateAll();
+        
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        String key = "test";
+        System.out.println(localCache.get(key));
+        localCache.refresh(key);
+        System.out.println(localCache.get(key));
+    }
+
+    public static void main(String[] args) {
+        GuavaCacheDemo demo = new GuavaCacheDemo();
+        demo.testPrintMatrix();
+    }
+
+    public void testPrintMatrix() {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        // 定义四个边界
+        int left = 0, right = cols - 1;
+        int top = 0, bottom = rows - 1;
+        
+        while (left <= right && top <= bottom) {
+            // 从左到右
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+            
+            // 从上到下
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+            
+            // 从右到左
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+            
+            // 从下到上
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
     }
 }
---
+++ commons/src/main/java/com/learning/java8/completablefuture/CustomException.java
@@ -0,0 +1,6 @@
+package com.learning.java8.completablefuture;/**
+ * @author xuechongyang
+ * @date 2024/5/21
+ */
+public class CustomException {
+}
